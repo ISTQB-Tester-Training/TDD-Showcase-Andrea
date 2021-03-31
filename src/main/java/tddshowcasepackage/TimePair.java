2064d4e1472a99bb794d7ea6948f141f3057ccad
cziegaus <54708551+cziegaus@users.noreply.github.com>
2021-03-31 12:52:46 +0200

    Commit nach Step 6

--- src/main/java/tddshowcasepackage/TimePair.java
+++ src/main/java/tddshowcasepackage/TimePair.java
@@ -8,9 +8,10 @@ public class TimePair {
     private String startTime = null;
     private String endTime = null;
 
-    public TimePair() {}
+    public TimePair() {
+    }
 
-    public void setTimeValues (String strStartTime, String strEndTime) {
+    public void setTimeValues(String strStartTime, String strEndTime) {
         startTime = strStartTime;
         endTime = strEndTime;
     }
@@ -21,6 +22,32 @@ public class TimePair {
     }
 
     public double getTimeDifference() {
-        return LocalTime.parse(startTime).until(LocalTime.parse(endTime), ChronoUnit.MINUTES)/60.0;
+
+        try {
+            LocalTime checkStartTime = LocalTime.parse(startTime);
+        } catch (Exception e) {
+            throw new TimePairException(e.getMessage() + "(Input value: " + startTime + ")", 501);
+        }
+
+        return LocalTime.parse(startTime).until(LocalTime.parse(endTime), ChronoUnit.MINUTES) / 60.0;
+    }
+
+    public class TimePairException extends RuntimeException {
+        private final String message;
+        private final Long messageNr;
+
+        public TimePairException(String argMessage, long argMessageNr) {
+            super(argMessage);
+            message = argMessage;
+            messageNr = argMessageNr;
+        }
+
+        public String getMessageText() {
+            return message;
+        }
+
+        public Long getMessageNr() {
+            return messageNr;
+        }
     }
 }
