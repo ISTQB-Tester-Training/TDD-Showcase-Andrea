40e90d279a022a7781a804a9fe5db12930d303a7
cziegaus <54708551+cziegaus@users.noreply.github.com>
2021-03-31 12:58:36 +0200

    Commit nach Step 7

--- src/main/java/tddshowcasepackage/TimePair.java
+++ src/main/java/tddshowcasepackage/TimePair.java
@@ -8,10 +8,9 @@ public class TimePair {
     private String startTime = null;
     private String endTime = null;
 
-    public TimePair() {
-    }
+    public TimePair() {}
 
-    public void setTimeValues(String strStartTime, String strEndTime) {
+    public void setTimeValues (String strStartTime, String strEndTime) {
         startTime = strStartTime;
         endTime = strEndTime;
     }
@@ -23,31 +22,47 @@ public class TimePair {
 
     public double getTimeDifference() {
 
-        try {
-            LocalTime checkStartTime = LocalTime.parse(startTime);
-        } catch (Exception e) {
+        if (startTime == null || endTime == null) {
+            throw new TimePairException("Start oder End Time not set", 500);
+        }
+
+        try { LocalTime checkStartTime = LocalTime.parse(startTime);
+        }
+        catch (Exception e) {
             throw new TimePairException(e.getMessage() + "(Input value: " + startTime + ")", 501);
         }
 
-        return LocalTime.parse(startTime).until(LocalTime.parse(endTime), ChronoUnit.MINUTES) / 60.0;
+        try { LocalTime checkEndTime = LocalTime.parse(endTime);
+        }
+        catch (Exception e) {
+            throw new TimePairException(e.getMessage() + "(Input value: " + endTime + ")", 502);
+        }
+
+        double aTimeDifference = LocalTime.parse(startTime).until(LocalTime.parse(endTime), ChronoUnit.MINUTES)/60.0;
+        if (aTimeDifference < 0.0) {
+            throw new TimePairException("Invalid time period " +
+                    startTime + " / " +
+                    endTime +
+                    " time difference " + aTimeDifference + " hours", 503);
+        }
+        return aTimeDifference;
     }
 
     public class TimePairException extends RuntimeException {
         private final String message;
         private final Long messageNr;
 
-        public TimePairException(String argMessage, long argMessageNr) {
+        public TimePairException (String argMessage, long argMessageNr) {
             super(argMessage);
             message = argMessage;
             messageNr = argMessageNr;
         }
-
         public String getMessageText() {
             return message;
         }
-
         public Long getMessageNr() {
             return messageNr;
         }
     }
 }
+
